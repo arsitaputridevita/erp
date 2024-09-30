006070783305b8fc560ca1f2c0f4aa30e6e253e4
Ridhopajri <ridhofajriawan16@gmail.com>
2024-09-30 14:52:19 +0700

    tes commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,6 +14,7 @@ $routes->post('/login', 'Auth::login');
 $routes->get('/logout', 'Auth::logout');
 
 //routes dashboard
+// tes commit
 $routes->get('/dashboard', 'Dashboard', ['filter' => 'auth']);
 $routes->get('/masterdata/jenistes', 'sdm\JenisTes\JenisTes::index');
 $routes->get('/masterdata/jenistes/create', 'sdm\JenisTes\JenisTes::create', ['filter' => 'auth']);
